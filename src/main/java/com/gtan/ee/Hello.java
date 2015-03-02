9a46968ef5f00492534f02c6a5da065f5ff2bd1d
xiefei <xiefei@gtan.com>
2015-03-02 19:03:35 +0800

    introduce omnifaces

--- src/main/java/com/gtan/ee/Hello.java
+++ src/main/java/com/gtan/ee/Hello.java
@@ -1,13 +1,12 @@
 package com.gtan.ee;
 
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.faces.bean.SessionScoped;
-import java.util.logging.Level;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 import java.util.logging.Logger;
 
-@ManagedBean
+@Named
+@RequestScoped
 public class Hello {
     static Logger logger = Logger.getLogger(Hello.class.getName());
 
