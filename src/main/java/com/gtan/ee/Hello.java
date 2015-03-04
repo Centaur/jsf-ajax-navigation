49c66879ab80f253c32556a35b93fd6071c9605f
xiefei <xiefei@gtan.com>
2015-03-04 12:02:43 +0800

    mojarra bug

--- src/main/java/com/gtan/ee/Hello.java
+++ src/main/java/com/gtan/ee/Hello.java
@@ -1,7 +1,12 @@
 package com.gtan.ee;
 
 
+import org.omnifaces.util.Components;
+
 import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ComponentSystemEvent;
 import javax.inject.Named;
 import java.util.logging.Logger;
 
@@ -12,10 +17,29 @@ public class Hello {
 
     String name = "xiefei";
 
+    private String[] partials = new String[]{"partial1.xhtml", "partial2.xhtml"};
+    private String oldLinkId;
+
+
+    public String getOldLinkId() {
+        return oldLinkId;
+    }
+
+    public void setOldLinkId(String oldLinkId) {
+        this.oldLinkId = oldLinkId;
+    }
+
+    public String[] getPartials() {
+        return partials;
+    }
+
+    private String currentPartial = "partial1.xhtml";
+
     public String getName() {
         logger.fine("text get");
         return name;
     }
+
 
     public void setName(String name) {
         this.name = name;
@@ -23,5 +47,17 @@ public class Hello {
 
     public void log() {
         logger.fine("text changed");
+    }
+
+    public void setCurrentPartial(String currentPartial) {
+        this.currentPartial = currentPartial;
+    }
+
+    public String getCurrentPartial() {
+        return currentPartial;
+    }
+
+    public void rememberOldLinkId(ComponentSystemEvent event) throws AbortProcessingException{
+        oldLinkId = ((UIComponent)event.getSource()).getClientId();
     }
 }
